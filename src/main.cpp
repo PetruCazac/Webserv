b392e47e51ed837a7fceb43c0947dd494ab747bd
pcazac <pcazac@student.42heilbronn.de>
2024-04-22 11:33:28 +0200

    Commented the main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 // #include "Logger.hpp"
 
-int main() {
-    Logger::setLogLevel(DEBUG);
-    LOG_INFO("Hello, world!");
-    LOG_DEBUG("This is a debug message.");
-    LOG_ERROR("This is an error message.");
-    LOG_WARNING("This is a warning message.");
-    return 0;
-}
+// int main() {
+//     Logger::setLogLevel(DEBUG);
+//     LOG_INFO("Hello, world!");
+//     LOG_DEBUG("This is a debug message.");
+//     LOG_ERROR("This is an error message.");
+//     LOG_WARNING("This is a warning message.");
+//     return 0;
+// }
