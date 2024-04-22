e6f9cebc72176e5f92581d0c1621e1cdbf345ded
pcazac <pcazac@student.42heilbronn.de>
2024-04-22 15:06:53 +0200

    change translate Directives fct

--- src/config/Config.cpp
+++ src/config/Config.cpp
@@ -243,7 +243,6 @@ std::string translateDirectives(enum Parser directive){
 			return "ma_data_size_incoming";
 		case LOG_LEVEL:
 			return "log_level";
-		case TOTAL:
-			return NULL;
+		return NULL;
 	}
 }
