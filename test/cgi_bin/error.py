52992fce6f6aeebccbf2e86952e42323612fc694
42fabileo <76965192+leonfabi@users.noreply.github.com>
2024-06-01 17:41:45 +0200

    wrong scripts

---
+++ test/cgi_bin/error.py
@@ -0,0 +1,30 @@
+import os
+import cgi
+s df ds s  a 
+
+def main():
+	# Get the query string from the environment variable
+	query_string = os.getenv('QUERY_STRING')
+	
+	# Parse the query string
+	form = cgi.FieldStorage()
+	
+	# Extract the 'name' and 'surname' parameters
+	name = form.getvalue('name', 'Stranger')
+	surname = form.getvalue('surname', '')
+	# Print the HTTP headers
+	print()  # End of headers
+	
+	# Print the HTML content
+	print("<html>")
+	print("<head><title>Hello</title></head>")
+	print("<body>")
+	print("<h1>Hello, {} {}</h1>".format(name, surname))
+	# for name, value in os.environ.items():
+	# 	print("{0}: {1}".format(name, value))
+	# print(os.environ)
+	print("</body>")
+	print("</html>")
+
+if __name__ == "__main__":
+	main()
